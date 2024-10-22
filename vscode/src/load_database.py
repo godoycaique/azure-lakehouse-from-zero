11cdcc15fba132b3fe650be0fd79e014aac8c070
Caique Godoy <caiquecgodoy@gmail.com>
2024-10-21 21:05:47 -0300

    update columns

--- vscode/src/load_database.py
+++ vscode/src/load_database.py
@@ -21,22 +21,35 @@ DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DATABASE_URL)
 
 
+def normalize_columns(df):
+    df.columns = (
+        df.columns.str.normalize('NFKD')
+        .str.replace(' ', "_", regex=True)
+        .str.replace('-', "_", regex=True)
+        .str.encode('ascii', errors='ignore')
+        .str.decode('utf-8')
+        .str.lower()
+        .str.strip()
+    )
+    return df
+
 def load_csv_database():
     file_path = 'data/sales_supermarket.csv'
     df = pd.read_csv(file_path, sep=',', quotechar='"')
     df = df.dropna(subset=['Order ID']).drop(columns=['Row ID'])
+    df = normalize_columns(df)
     return df
 
 def get_tables(df, schema):
-    customers_df = df[['Customer ID', 'Customer Name', 'Segment', 'Country', 'State', 'Postal Code', 'Region']].drop_duplicates().reset_index(drop=True)
+    customers_df = df[['customer_id', 'customer_name', 'segment', 'country', 'state', 'postal_code', 'region']].drop_duplicates().reset_index(drop=True)
     customers_df.to_sql('dim_customers', engine, if_exists='replace', index=True, schema=schema)
     print(f' > Tabela dim_customer carregada com {customers_df.count()} linhas')
 
-    products_df = df[['Product ID', 'Product Name', 'Category', 'Sub-Category']].drop_duplicates().reset_index(drop=True)
+    products_df = df[['product_id', 'product_name', 'category', 'sub_category']].drop_duplicates().reset_index(drop=True)
     products_df.to_sql('dim_products', engine, if_exists='replace', index=True, schema=schema)
     print(f' > Tabela dim_products carregada com {products_df.count()} linhas')
 
-    sales_df = df[['Order ID', 'Order Date', 'Ship Date', 'Ship Mode', 'Customer ID', 'Product ID', 'Sales']].drop_duplicates().reset_index(drop=True)
+    sales_df = df[['order_id', 'order_date', 'ship_date', 'ship_mode', 'customer_id', 'product_id', 'sales']].drop_duplicates().reset_index(drop=True)
     sales_df.to_sql('fact_sales', engine, if_exists='replace', index=True, schema=schema)
     print(f' > Tabela fact_sales carregada com {sales_df.count()} linhas')
 
